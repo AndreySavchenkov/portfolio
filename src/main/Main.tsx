dcac55f6ebe0fd4fc769969b180ab45dbc366e62
sandvik16 <sannndvik16@gmail.com>
2021-10-25 19:21:39 +0300

    styling header

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import style from './Main.module.scss'
 import styleContainer from './../common/styles/Container.module.css'
+import myPhoto from "../assets/image/myPhoto.webp";
+
 
 export const Main = () => {
+
+    const backgroundPhoto = {
+        backgroundImage: `url(${myPhoto})`,
+    }
+
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
@@ -11,7 +18,7 @@ export const Main = () => {
                     <h1>I'm Andrey Savchenkov</h1>
                     <p>Frontend Developer</p>
                 </div>
-                <div className={style.photo}>
+                <div className={style.photo} style={backgroundPhoto}>
 
                 </div>
             </div>
