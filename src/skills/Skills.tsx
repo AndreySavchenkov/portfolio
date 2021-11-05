862ae80d97b3938b1798de5e6768fbc399883e25
sandvik16 <sannndvik16@gmail.com>
2021-11-05 23:10:07 +0300

    add skills

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -10,11 +10,15 @@ export const Skills = () => {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title text={'Skills'}/>
                 <div className={style.skills}>
+                    <Skill title={'React'}
+                           description={'information about skill'}/>
+                    <Skill title={'Typescript'}
+                           description={'information about skill'}/>
+                    <Skill title={'JavaScript'}
+                           description={'information about skill'}/>
                     <Skill title={'HTML'}
                            description={'information about skill'}/>
                     <Skill title={'CSS'}
-                           description={'information about skill'}/>
-                    <Skill title={'JavaScript'}
                            description={'information about skill'}/>
                 </div>
             </div>
