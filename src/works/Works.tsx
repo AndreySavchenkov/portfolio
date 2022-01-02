8bb478941678fae9255915f77177a46e2a73afb6
sandvik16 <sannndvik16@gmail.com>
2022-01-02 16:32:09 +0300

    added work

--- src/works/Works.tsx
+++ src/works/Works.tsx
@@ -5,6 +5,7 @@ import styleContainer from './../common/styles/Container.module.css'
 import {Title} from "../common/components/Title/Title";
 import todoImage from './../assets/image/toDoList.jpg'
 import socialImage from './../assets/image/socialNetwork.jpg'
+import yesNo from './../assets/image/yes-no.jpg'
 
 export const Works = () => {
 
@@ -21,8 +22,18 @@ export const Works = () => {
             <div className={`${styleContainer.container} ${style.container}`}>
                 <Title text={'Projects'}/>
                 <div className={style.worksContainer}>
-                    <Work style={social} title={'Social Network'} text={'some text about project'}/>
-                    <Work style={todoList} title={'Todo List'} text={'some text about project'}/>
+                    <Work link={'https://andreysavchenkov.github.io/Social-Network/'}
+                          style={social}
+                          title={'Social Network'}
+                          text={'HTML, SCSS, React, TypeScript, Redux, Axios, REST API, Formik'}/>
+                    <Work link={'https://andreysavchenkov.github.io/ToDoMain/'}
+                          style={todoList}
+                          title={'ToDo List'}
+                          text={'HTML, SCSS, React, TypeScript, Redux, Axios, REST API, MaterialUI, Formik'}/>
+                    <Work link={'https://andreysavchenkov.github.io/YesNoMaybe/'}
+                          style={yesNo}
+                          title={'Yes-No-Maybe ;)'}
+                          text={'HTML, MaterialUI, React, TypeScript, Axios, REST API'}/>
                 </div>
             </div>
         </div>
--- src/works/work/Work.tsx
+++ src/works/work/Work.tsx
@@ -5,17 +5,18 @@ export type WorkType = {
     title: string,
     text: string,
     style: {},
+    link: string;
 }
 
 export const Work = (props: WorkType) => {
     return (
         <div className={style.work}>
             <div className={style.image} style={props.style}>
-                <a href='#' className={style.viewBtn}>See</a>
+                <a href={props.link} className={style.viewBtn}>See</a>
             </div>
             <div className={style.textContainer}>
                 <span className={style.projectTitle}>{props.title}</span>
-                <p className={style.text}>{props.text}</p>
+                <p className={style.text}>Technology Stack: {props.text}</p>
             </div>
 
         </div>
