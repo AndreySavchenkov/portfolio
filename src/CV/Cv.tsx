e612baf93386f010b2830456cecd823d22b4157a
sandvik16 <sannndvik16@gmail.com>
2022-05-02 12:05:37 +0300

    fix: change cv link

--- src/CV/Cv.tsx
+++ src/CV/Cv.tsx
@@ -5,7 +5,7 @@ import { JackInTheBox } from 'react-awesome-reveal';
 export const Cv = () => (
   <JackInTheBox duration={4000}>
     <div className={style.cvContainer}>
-      <a href={'https://drive.google.com/file/d/1Nu-31snnE93jHnon6PADa91wXK6e6lnd/view?usp=sharing'}
+      <a href={'https://drive.google.com/file/d/1tn8RSLT1Vimgqyuj0TF7xmqhiollN1Na/view?usp=sharing'}
          target={'_blank'} download={'AndrewCV.pdf'}>Download My CV</a>
     </div>
   </JackInTheBox>
